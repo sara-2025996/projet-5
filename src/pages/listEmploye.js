07e5528929702fe99d8835f1b9cf7c288472d897
sara-2025996 <sara.bouzekri2006@gmail.com>
2025-12-12 18:42:06 +0100

    modList

--- src/pages/listEmploye.js
+++ src/pages/listEmploye.js
@@ -1,71 +1,80 @@
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { getEmployees, RemoveEmployee } from "../redux/employeSlice"
-import { useNavigate } from "react-router-dom"
-import { Link } from "react-router-dom"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getEmployees, RemoveEmployee } from "../redux/employeSlice";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../component/navbar";
 
 
+const List = () => {
+  const employees = useSelector((state) => state.employees.list);
+  const nbr=employees.length
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(getEmployees());
+  }, [dispatch]);
+
+  const supprimer = (id) => {
+    dispatch(RemoveEmployee(id));
+  };
+
+  return (
+    <>
+    <Navbar/>
+
+    <div className="container-xl mt-4">
+        <h2 className="text-center">list des employees ({nbr})
+
+        </h2>
+        <button onClick={() => navigate(`/employees/add`)} className="btn btn-outline-primary mb-2">
+          Ajouter
+        </button>
+    
+      <div className="table">
+        <table className="table table-hover">
+          <thead className="table-secondary">
+            <tr>
+              <th>Nom</th>
+              <th>Prénom</th>
+              <th>Poste</th>
+              <th>Département</th>
+             
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            
+            {employees.map((m) => (
+              <tr key={m.id}  onClick={() => navigate(`/employees/detail/${m.id}`)}  >
+                <td>{m.nom}</td>
+                <td>{m.prenom}</td>
+                <td>{m.poste}</td>
+                <td>{m.departement}</td>
+              
+                
+                <td className="d-flex flex-wrap ">
+                  
+                
+                 
+                  <button onClick={(e) =>{e.stopPropagation(); supprimer(m.id)}} className="btn btn-danger ">
+                    Supprimer
+                  </button>
+
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      
+      </div>
 
 
+    </>
 
+  );
+};
 
-const List=()=>{
-    const employees=useSelector(state=>state.employees.list)
-
-    const dispatch=useDispatch()
-    const navigat=useNavigate()
-
-    useEffect(()=>{
-        dispatch(getEmployees())
-    },[dispatch])
-
-    const supprimer=(id)=>{
-        dispatch(RemoveEmployee(id))
-    }
-
-
-    return(
-        <div className="container" >
-            <button onClick={()=>navigat(`/employees/add`)} className="btn btn-outline-primary mt-4 ">ajouter</button>
-
-            <table className="table table-hover text-center mt-3" >
-                <thead>
-                    <tr>
-                        <th>nom</th>
-                        <th>prenom</th>
-                        <th>post</th>
-                        <th>departement</th>
-                        <th>salaire</th>
-                        <th>dateEmbauche</th>
-                        <th>email</th>
-                        <th>telephone</th>
-                        <th>actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {employees.map(m=>(
-                        <tr key={m.id}>
-                            <td>{m.nom}</td>
-                            <td>{m.prenom}</td>
-                            <td>{m.post}</td>
-                            <td>{m.departement}</td>
-                            <td>{m.salaire}</td>
-                            <td>{m.dateEmbauche}</td>
-                            <td>{m.email}</td>
-                            <td>{m.telephone}</td>
-                            <td >
-                                <button onClick={()=>navigat(`/employees/detail/${m.id}`)} className="btn btn-primary  " >voir</button>
-                                <Link to={`/employees/edite/${m.id}`} >
-                                <button className="btn btn-secondary ms-4 " >modifier</button>
-                                </Link>
-                                <button onClick={()=>supprimer(m.id)} className="btn btn-danger ms-4" >supprimer</button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-
-        </div>
-    )
-}
-export default List
+export default List;
